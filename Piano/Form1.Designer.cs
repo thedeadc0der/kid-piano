438a6e37c22b16aabef59548db1ea2fb4084ca02
= <deadc0der@mail.com>
2015-12-01 19:54:09 +0100

    Update main window

--- Piano/Form1.Designer.cs
+++ Piano/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pianoControl1 = new Piano.PianoControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MaximumSize = new System.Drawing.Size(0, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(838, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 60);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(838, 271);
+            this.splitContainer1.SplitterDistance = 279;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.CausesValidation = false;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Mode 1",
+            "Mode 2",
+            "Mode 3",
+            "Mode ...",
+            "Mode N"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.MaximumSize = new System.Drawing.Size(240, 0);
+            this.listBox1.MinimumSize = new System.Drawing.Size(240, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 271);
+            this.listBox1.TabIndex = 0;
+            // 
+            // pianoControl1
+            // 
+            this.pianoControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pianoControl1.Location = new System.Drawing.Point(0, 331);
+            this.pianoControl1.MaximumSize = new System.Drawing.Size(0, 128);
+            this.pianoControl1.Name = "pianoControl1";
+            this.pianoControl1.Size = new System.Drawing.Size(838, 128);
+            this.pianoControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(838, 459);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pianoControl1);
+            this.Name = "Form1";
+            this.Text = "Piano";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PianoControl pianoControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
