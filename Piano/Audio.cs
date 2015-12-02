2c213cc5567c5ddc5c66c32d3e994abec361ed37
= <deadc0der@mail.com>
2015-12-02 22:45:36 +0100

    Add Audio class skeleton.

---
+++ Piano/Audio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piano
+{
+    class Audio
+    {
+        public Audio()
+        {
+
+        }
+
+        public void JouerNote( Note note)
+        {
+
+        }
+    }
+}
