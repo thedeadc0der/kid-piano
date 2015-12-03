2c9c8b0691e7be1dd3c0c4f26b1258f5e0713823
vidoguef <francois.vidogue@hotmail.fr>
2015-12-03 14:01:44 +0100

    JouerNote operationnel (sauf pour les Dieses)

--- Piano/Audio.cs
+++ Piano/Audio.cs
@@ -18,18 +18,18 @@ namespace Piano
         {
             switch (note.Nom)
             {
-                case NomNote.Do:using (var soundPlayer = new SoundPlayer(@"c:\Windows\Media\chimes.wav")){soundPlayer.Play();}; break;
+                case NomNote.Do:using (var soundPlayer = new SoundPlayer(Properties.Resources._do)){soundPlayer.Play();}; break;
                 case NomNote.DoDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Fa: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Fa: using (var soundPlayer = new SoundPlayer(Properties.Resources.fa)) { soundPlayer.Play(); }; break;
                 case NomNote.FaDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Invalide: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.La: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Invalide: ; break;
+                case NomNote.La: using (var soundPlayer = new SoundPlayer(Properties.Resources.la)) { soundPlayer.Play(); }; break;
                 case NomNote.LaDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Mi: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Re: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Mi: using (var soundPlayer = new SoundPlayer(Properties.Resources.mi)) { soundPlayer.Play(); }; break;
+                case NomNote.Re: using (var soundPlayer = new SoundPlayer(Properties.Resources.re)) { soundPlayer.Play(); }; break;
                 case NomNote.ReDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Si: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
-                case NomNote.Sol: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Si: using (var soundPlayer = new SoundPlayer(Properties.Resources.si)) { soundPlayer.Play(); }; break;
+                case NomNote.Sol: using (var soundPlayer = new SoundPlayer(Properties.Resources.sol)) { soundPlayer.Play(); }; break;
                 case NomNote.SolDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
 
             }
