1eddf395d5c398c916944eb5f0f4c84e3b1a1e3f
vidoguef <francois.vidogue@hotmail.fr>
2015-12-03 13:44:20 +0100

    JouerNote(Note note)    (Switch)

--- Piano/Audio.cs
+++ Piano/Audio.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,22 @@ namespace Piano
 
         public void JouerNote( Note note)
         {
+            switch (note.Nom)
+            {
+                case NomNote.Do:using (var soundPlayer = new SoundPlayer(@"c:\Windows\Media\chimes.wav")){soundPlayer.Play();}; break;
+                case NomNote.DoDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Fa: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.FaDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Invalide: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.La: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.LaDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Mi: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Re: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.ReDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Si: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.Sol: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
+                case NomNote.SolDiese: using (var soundPlayer = new SoundPlayer()) { soundPlayer.Play(); }; break;
 
-        }
+            }
     }
 }
